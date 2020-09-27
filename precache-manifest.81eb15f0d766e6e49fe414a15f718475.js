9b519deb62b2fca5bd6b091af5a17d30c6183509
harry-chiu <harrychiu1106@gmail.com>
2020-09-28 00:16:43 +0800

    Updates

---
+++ precache-manifest.81eb15f0d766e6e49fe414a15f718475.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69a2b6eab012d482a9264d84a8d0cd8f",
+    "url": "/have-a-good-time/index.html"
+  },
+  {
+    "revision": "26fc213403891d454a34",
+    "url": "/have-a-good-time/static/js/2.da93b6fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/have-a-good-time/static/js/2.da93b6fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deaa35b9af6cefb95d0d",
+    "url": "/have-a-good-time/static/js/main.00d792d0.chunk.js"
+  },
+  {
+    "revision": "334490e0c85c496e9e54",
+    "url": "/have-a-good-time/static/js/runtime-main.7a080d0d.js"
+  },
+  {
+    "revision": "c7e587b86afae401c8900e1a32d620ea",
+    "url": "/have-a-good-time/static/media/building-1.c7e587b8.svg"
+  },
+  {
+    "revision": "642381be42351e62810d6e93df848297",
+    "url": "/have-a-good-time/static/media/building-2.642381be.svg"
+  },
+  {
+    "revision": "4f27f29cb556103a0850cdd0238b58e3",
+    "url": "/have-a-good-time/static/media/building-3.4f27f29c.svg"
+  },
+  {
+    "revision": "24520a1bb8c4aa5f2c18cab7d7a61914",
+    "url": "/have-a-good-time/static/media/building-4.24520a1b.svg"
+  },
+  {
+    "revision": "e56667f374850924ef536935db5d7291",
+    "url": "/have-a-good-time/static/media/cloud-1.e56667f3.svg"
+  },
+  {
+    "revision": "bdfd90660e255931a9574cc6c4371fbd",
+    "url": "/have-a-good-time/static/media/cloud-2.bdfd9066.svg"
+  }
+]);
